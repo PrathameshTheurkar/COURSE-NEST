6b696b7305570350ae8af491c2f20047e4e8dd63
Prathamesh Theurkar <prathameshtheurkar037@gmail.com>
2024-05-21 01:10:42 +0530

    added env variables

--- backend-course-app/index.js
+++ backend-course-app/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const adminRouter = require('./routes/admin.js')
 const userRouter = require('./routes/user.js')
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors())
@@ -18,10 +19,11 @@ app.use('/users', userRouter)
 
 // Connect to MongoDB
 // mongoose.connect('mongodb+srv://prathameshtheurkar037:Prathamesh%401@cluster0.s8asa1j.mongodb.net/' ,  { useNewUrlParser: true, useUnifiedTopology: true, dbName: "course-selling-app" })
-mongoose.connect('mongodb+srv://prathameshtheurkar037:Prathamesh%401@cluster0.s8asa1j.mongodb.net/' ,  { dbName: "course-selling-app" })
+// mongoose.connect('mongodb+srv://prathameshtheurkar037:Prathamesh%401@cluster0.s8asa1j.mongodb.net/' ,  { dbName: "course-selling-app" })
+mongoose.connect(process.env.MONGODB_URL ,  { dbName: process.env.DB_NAME })
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is listening on port 3000');
 });
 
